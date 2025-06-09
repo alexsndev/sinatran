f66bc159f21cc21840635fe57dd38d3ddd4bb76e
liz-b2b <soaares.lizandra@gmail.com>
2025-06-09 12:23:21 -0300

    nova tentativa icon de biblioteca

--- resources/views/layouts/components/navbar.blade.php
+++ resources/views/layouts/components/navbar.blade.php
@@ -10,12 +10,15 @@
         </a>
 
         {{-- Botão Mobile (Hamburguer) --}}
+        {{-- Usando Heroicons (https://heroicons.com/) via SVG inline --}}
         <button id="navbar-toggle"
                 class="sm:hidden flex items-center justify-center p-0 m-0 rounded text-black focus:outline-none z-50"
                 aria-label="Abrir menu"
                 style="margin-right: 0; background: none; border: none;">
-            {{-- SVG externo: menu hamburguer usando imagem SVG baixada --}}
-            <img src="{{ asset('images/menu.svg') }}" alt="Menu" class="w-10 h-10" draggable="false" />
+            {{-- Heroicon: Bars-3 --}}
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16"/>
+            </svg>
         </button>
 
         {{-- Menu de Navegação Desktop --}}
